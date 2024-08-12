c1fca243dcd940821dc39df6b9c28535afc39d21
harijoshi <hariojoshi@gmail.com>
2024-08-12 23:44:19 +0545

    chore: add internet permissions

---
+++ app/src/main/java/com/example/kotlin_webview/WebViewScreen.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_webview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WebViewScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun WebViewScreenPreview() {
+    WebViewScreen()
+}
