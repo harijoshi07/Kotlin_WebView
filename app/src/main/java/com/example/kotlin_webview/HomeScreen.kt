18bc7ff95b50d44f245eb7439f0cd5167cd19b46
harijoshi <hariojoshi@gmail.com>
2024-08-14 21:16:57 +0545

    feat: impl spalsh screen

--- app/src/main/java/com/example/kotlin_webview/HomeScreen.kt
+++ app/src/main/java/com/example/kotlin_webview/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.kotlin_webview
 
+import android.annotation.SuppressLint
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -11,33 +14,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier) {
 
     val url by remember {
-        mutableStateOf("https://www.github.com/harijoshi07")
+        mutableStateOf("https://www.youtube.com")
     }
 
     val webView by remember {
         mutableStateOf<WebView?>(null)
     }
+    Box(modifier = Modifier.fillMaxSize()) {
+        WebViewScreen(url, webView)
+    }
+}
+
+
+@Composable
+fun WebViewScreen(
+    url:String,
+    webView: WebView?,
+    modifier: Modifier = Modifier
+) {
 
     AndroidView(factory = { context ->
         WebView(context).apply {
             settings.javaScriptEnabled = true
             webViewClient = WebViewClient()
             loadUrl(url)
-
         }
     }, update = {
         webView?.loadUrl(url)
     }
     )
-
 }
+
+
 
 @Preview
 @Composable
-private fun WebViewScreenPreview() {
-    WebViewScreen()
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
--- app/src/main/java/com/example/kotlin_webview/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_webview/MainActivity.kt
@@ -7,20 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlin_webview.ui.theme.Kotlin_WebViewTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Kotlin_WebViewTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    WebViewScreen(modifier = Modifier.padding(innerPadding))
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
